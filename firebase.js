ec6f4210324b3d2f6f5fdeb994886810d1381775
Minsuh Roh <127650200+rohgun@users.noreply.github.com>
2024-10-09 00:55:02 +0900

    change

---
+++ firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+import {getAuth} from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DB_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
